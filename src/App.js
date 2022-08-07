1ddda455111ced2e08a38156cafc91e948c51290
inzamam1 <inzamamsayyed10@gmail.com>
2022-08-07 20:26:16 +0530

    Rendering profile and footer in home -> !app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-import Profile from "./Portfolio_Container/Home/Profile";
-
+import Home from "./Portfolio_Container/Home/Home";
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <Home />
     </div>
   );
 }
