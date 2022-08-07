fbc8211faa80720a8aa753fd2121d7312fd74cde
inzamam1 <inzamamsayyed10@gmail.com>
2022-08-07 20:26:46 +0530

    Added curve ending to the section

---
+++ src/Portfolio_Container/Home/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+export default function Footer() {
+  return (
+    <div className="footer_container">
+      <div className="footer_parent">
+        <img
+          src={require("../../../assets/Home/shape-bg.png")}
+          alt="no internet connection"
+        />
+      </div>
+    </div>
+  );
+}
