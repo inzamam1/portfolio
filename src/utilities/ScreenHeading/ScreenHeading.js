931cf1193e16686e483353dd48e60e99b18bfce6
inzamam1 <inzamamsayyed10@gmail.com>
2022-11-06 19:13:03 +0530

    fixed blob css

--- src/utilities/ScreenHeading/ScreenHeading.js
+++ src/utilities/ScreenHeading/ScreenHeading.js
@@ -15,11 +15,10 @@ export default function ScreenHeading(props) {
         <div></div>
       )}
       <div className="heading-seperator">
-        <div className="seperator-line">
-          <div className="seperator-blob">
-            <div></div>
-          </div>
+        <div className="seperator-blob">
+          <div></div>
         </div>
+        <div className="seperator-line"></div>
       </div>
     </div>
   );
